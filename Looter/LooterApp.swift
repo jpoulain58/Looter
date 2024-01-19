5f7078eb2cc51d1fe94b7cc861c0b224aa424d44
Jerem <jpoulain58@gmail.com>
2024-01-19 10:31:37 +0100

    Initial Commit

---
+++ Looter/LooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooterApp.swift
+//  Looter
+//
+//  Created by Jeremy POULAIN on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
